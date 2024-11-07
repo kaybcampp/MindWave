41cf9374ca995d165f40bddac7f1332bbb1bf3dd
kayb <132619597+kaybcampp@users.noreply.github.com>
2024-11-07 17:10:40 -0500

    updated loading page from home to chat

--- homePage/home.js
+++ homePage/home.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     chatIcon.addEventListener("click", (event) => {
         event.preventDefault(); // Prevent direct link navigation
 
+        // Save the target page (chat page) in session storage to retrieve it from loading page
+        sessionStorage.setItem("targetPage", "../chatPage/chat.html");
+
         // Redirect to the loading page
         window.location.href = "../loadingPage/loading.html";
     });
